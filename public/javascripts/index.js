436c7c947f9befaad0be3483eaa22a26cffee95e
Harryyoshi <75763081+Harryyoshi@users.noreply.github.com>
2021-09-18 09:05:50 -0400

    Add existing work

---
+++ public/javascripts/index.js
@@ -0,0 +1,6 @@
+function siteDirector() {
+    alert("your mom");
+}
+function displayResult() {
+    document.getElementById("myHeader").innerHTML = "Have a nice day!";
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,72 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+const express = require('express')
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "fengzhang",
+  password: "#Zyf340982495"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+//This can read and write from database VERY IMPORTANT
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Result: " + result);
+    });
+  });
+
+
+//Creates a database
+con.connect(function(err) {
+if (err) throw err;
+console.log("Connected!");
+con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+});
+});
+
+/* Creates a table in a database
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword",
+    database: "mydb"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
+  });
+
+  //INSERT INTO TABLES
+  con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
+  */
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
